e8d5aef843577f96379a531b6983ef061835e917
Juan Diego <djteniente@hotmail.com>
2019-05-19 21:04:32 -0500

    Initialization

---
+++ pages/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from .views import PageListView, PageDetailView, PageCreateView, PageUpdate, PageDelete
+
+pages_patterns = ([
+    path('', PageListView.as_view(), name='pages'),
+    path('<int:pk>/<slug:slug>/', PageDetailView.as_view(), name='page'),
+    path('create/', PageCreateView.as_view(), name="create"),  
+    path('update/<int:pk>/', PageUpdate.as_view(), name="update"),
+    path('delete/<int:pk>', PageDelete.as_view(), name="delete"),
+], 'pages')
