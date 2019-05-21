2f0542d816964ce344a2ff2a7e99f155dd576dd9
Juan Diego <djteniente@hotmail.com>
2019-05-21 06:27:06 -0500

    Ajustes autenticacion

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
 from django.urls import reverse, reverse_lazy
 from .models import Page
+from .forms import PageForm
 
 # Create your views here.
 class PageListView(ListView):
@@ -12,19 +15,22 @@ class PageListView(ListView):
 class PageDetailView(DetailView):
     model = Page
 
+@method_decorator(staff_member_required,name='dispatch')
 class PageCreateView(CreateView):
     model = Page
-    fields = ['title', 'content', 'order']
+    form_class = PageForm
     success_url = reverse_lazy('pages:pages')
 
+@method_decorator(staff_member_required,name='dispatch')
 class PageUpdate(UpdateView):
     model = Page
-    fields = ['title', 'content', 'order']
+    form_class = PageForm
     template_name_suffix = '_update_form'
 
     def get_success_url(self):
         return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
-
+           
+@method_decorator(staff_member_required,name='dispatch')
 class PageDelete(DeleteView):
     model = Page
     success_url = reverse_lazy('pages:pages')
